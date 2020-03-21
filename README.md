0f6336cf38f964afced7e22192e324d30fa1141f
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-03-21 18:42:09 +0100

    :tada: Hello Berlin :de:

---
+++ README.md
@@ -0,0 +1,32 @@
+# Berlin EN News
+
+Belin EN News Twitter bot. It fetches news from https://www.berlin.de/en/news/ and Tweets to [@Berlin_EN_News](https://twitter.com/berlin_en_news).
+
+## Installation
+
+```bash
+npm install
+```
+
+### Environment variables
+
+The following environment variables need to be present:
+
+#### Twitter
+
+| API key         | API secret key         | Access token         | Access token secret         |
+| --------------- | ---------------------- | -------------------- | --------------------------- |
+| TWITTER_API_KEY | TWITTER_API_SECRET_KEY | TWITTER_ACCESS_TOKEN | TWITTER_ACCESS_TOKEN_SECRET |
+
+## Usage
+
+```bash
+npm start
+```
+
+## Contributing
+
+Please, check open issues. There are not guidelines so far 😅
+
+## License
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Twitter = require('twitter');
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+const placeId = '3078869807f9dd36'; // Berlin's place ID
+
+const BASE_URL = 'https://www.berlin.de';
+const NEWS_PATH = '/en/news/';
+
+async function fetchArticles() {
+  const response = await axios(`${BASE_URL}${NEWS_PATH}`);
+  const $ = cheerio.load(response.data);
+  // .special might include some "random" articles
+  const articles = $('#hnews').parent().find('article').not('.special').map(function() {
+    const heading = $(this).find('.heading');
+    return {
+      title: heading.text(),
+      link: `${BASE_URL}${heading.find('a').attr('href')}`,
+    };
+  }).toArray();
+
+  return articles;
+}
+
+async function fetchFirst5Articles() {
+  const articles = await fetchArticles();
+
+  return articles.slice(0,5);
+}
+
+async function postTweet(status) {
+  const response = await client.post('statuses/update', {
+    status,
+    place_id: placeId,
+  });
+
+  console.log(JSON.stringify(response));
+}
+
+async function homeTimeline() {
+  try {
+    const response = await client.get('statuses/user_timeline', {});
+    const responseTitles = response.map((tweet) => tweet.text.split('\n')[0]);
+    
+    return responseTitles;
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+(async function() {
+  const [articles, tweets] = await Promise.all([fetchFirst5Articles(), homeTimeline()]);
+  const newArticles = articles.filter(article => !tweets.includes(article.title));
+
+  for (const article of newArticles) {
+    const response = await postTweet([
+      article.title,
+      `Read more: ${article.link}`,
+    ].join('\n'));
+
+    console.log(response);
+  }
+})();
