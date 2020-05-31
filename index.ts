37ac55f059d44355cc4fe70b8f4768c0d77bb959
Mehmet Volkan Deveci <arayuzgelistirici@gmail.com>
2020-05-31 12:43:09 +0200

    feat(new source support): multiple source support added, berliner-zeitung source added
    I created an object as SOURCES to create a container for all sources instead of BASE_URL and
    NEWS_PATH, I refactored "fetchArticles" function, I used server names for separate the fragmentation
    functions by each source.

--- index.ts
+++ index.ts
@@ -12,33 +12,67 @@ const client = new Twitter({
 });
 const placeId = '3078869807f9dd36'; // Berlin's place ID
 
-const BASE_URL = 'https://www.berlin.de';
-const NEWS_PATH = '/en/news/';
+const SOURCES = [
+  {
+    server_name: 'berlin',
+    url: 'https://www.berlin.de',
+    news_path: '/en/news/',
+  },
+  {
+    server_name: 'berliner',
+    url: 'https://www.berliner-zeitung.de',
+    news_path: '/en/',
+  },
+];
 
-async function fetchArticles() {
-  const response = await axios(`${BASE_URL}${NEWS_PATH}`);
+async function fetchArticles(serverName, baseUrl, newsPath) {
+  const response = await axios(`${baseUrl}${newsPath}`);
   const $ = cheerio.load(response.data);
   // .special might include some "random" articles
-  const articles = $('#hnews')
-    .parent()
-    .find('article')
-    .not('.special')
-    .map(function () {
-      const heading = $(this).find('.heading');
-      return {
-        title: heading.text(),
-        link: `${BASE_URL}${heading.find('a').attr('href')}`,
-      };
-    })
-    .toArray();
+
+  let articles;
+  if (serverName === 'berliner') {
+    articles = $('.o-weekend')
+      .find('.m-teaser.m-teaser--medium')
+      .map(function () {
+        const heading = $(this).find('.m-teaser__title.js-m-teaser__title');
+        const topic = $(this)
+          .find('.m-teaser__title.js-m-teaser__title')
+          .find('span')
+          .text();
+        const url = $(this).find('.m-teaser.m-teaser--medium a');
+        return {
+          title: `${topic} => ${heading
+            .text()
+            .trim()
+            .replace(topic, '')
+            .replace(/\n/g, '')}`,
+          link: `${baseUrl}${url.attr('href')}`,
+        };
+      })
+      .toArray();
+  } else if (serverName === 'berlin') {
+    articles = $('#hnews')
+      .parent()
+      .find('article')
+      .not('.special')
+      .map(function () {
+        const heading = $(this).find('.heading');
+        return {
+          title: heading.text(),
+          link: `${baseUrl}${heading.find('a').attr('href')}`,
+        };
+      })
+      .toArray();
+  }
 
   console.log('Fetched articles: ', articles);
 
   return articles;
 }
 
-async function fetchFirst3Articles() {
-  const articles = await fetchArticles();
+async function fetchFirst3Articles(serverName, baseURL, newsPath) {
+  const articles = await fetchArticles(serverName, baseURL, newsPath);
 
   console.log('Selected 3 articles: ', articles);
 
@@ -51,8 +85,6 @@ async function postTweet({ status, media_ids }) {
     media_ids,
     place_id: placeId,
   });
-
-  return response;
 }
 
 async function postImage(image) {
@@ -94,23 +126,37 @@ async function fetchImageIds(urls) {
   }
 }
 
-async function fetchImageUrl(url) {
+async function fetchImageUrl(serverName, url) {
   try {
     const response = await axios(url);
     const $ = cheerio.load(response.data);
-    const imageElement = $(
-      '.main-content .page-mainimage,.main-content .swiper-wrapper',
-    ).find('img');
+    let imageElement, imageURls;
+    if (serverName === 'berliner') {
+      imageElement = $(
+        '.figure.c-image[data-component-name="Image"]:first-child',
+      ).find('img');
 
-    const imageURls = imageElement
-      .map(
-        (index, imageTag) =>
-          `${BASE_URL}${
-            $(imageTag).attr('data-src') || $(imageTag).attr('src')
-          }`,
-      )
-      .toArray()
-      .slice(0, 4);
+      imageURls = imageElement
+        .map(
+          (index, imageTag) =>
+            `${$(imageTag).attr('src') || $(imageTag).attr('data-src')}`,
+        )
+        .toArray()
+        .slice(0, 4);
+    } else if (serverName === 'berlin') {
+      imageElement = $(
+        '.main-content .page-mainimage,.main-content .swiper-wrapper',
+      ).find('img');
+      imageURls = imageElement
+        .map(
+          (index, imageTag) =>
+            `${SOURCES[0].url}${
+              $(imageTag).attr('data-src') || $(imageTag).attr('src')
+            }`,
+        )
+        .toArray()
+        .slice(0, 4);
+    }
 
     return imageURls;
   } catch (e) {
@@ -131,9 +177,9 @@ async function homeTimeline() {
   }
 }
 
-exports.handler = async function handler() {
+async function EngineStart(serverName, baseURl, newsPath) {
   const [articles, tweets] = await Promise.all([
-    fetchFirst3Articles(),
+    fetchFirst3Articles(serverName, baseURl, newsPath),
     homeTimeline(),
   ]);
   const newArticles = articles.filter(
@@ -142,18 +188,23 @@ exports.handler = async function handler() {
 
   const articlesWithImage = await Promise.all(
     newArticles.map(async (article) => {
-      article.imageUrls = await fetchImageUrl(article.link);
+      article.imageUrls = await fetchImageUrl(serverName, article.link);
       return article;
     }),
   );
 
   console.log('New articles: ', articlesWithImage);
-
   for (const article of articlesWithImage) {
     const status = [article.title, `Read more: ${article.link}`].join('\n');
     const media_ids = await fetchImageIds(article.imageUrls);
     const response = await postTweet({ status, media_ids });
 
     console.log('Tweet response: ', response);
+  }
+}
+
+exports.handler = async function handler() {
+  for (const source of SOURCES) {
+    EngineStart(source.server_name, source.url, source.news_path);
   }
 };
